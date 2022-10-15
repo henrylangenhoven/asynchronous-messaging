ce273232e6f523a8519e2a431623d0db0557e886
Henry Langenhoven <henry.l.pta@gmail.com>
2022-10-15 08:50:43 +0200

    test workflow

--- asynchronous-messaging-ui/src/environments/environment.prod.ts
+++ asynchronous-messaging-ui/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyBCDdhymBV26QJEi4se00eaujvh3Db8qwQ",
+    authDomain: "asynchronous-messaging.firebaseapp.com",
+    projectId: "asynchronous-messaging",
+    storageBucket: "asynchronous-messaging.appspot.com",
+    messagingSenderId: "951748759740",
+    appId: "1:951748759740:web:741a8058970993449440b9",
+    measurementId: "G-D9QQ9X48CD"
+  }
 };
